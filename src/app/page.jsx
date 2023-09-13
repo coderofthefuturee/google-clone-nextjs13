38f1e1a7297c86b8d07b302fa59214d2ef1ae011
Prince <princeking950@gmail.com>
2023-09-12 17:20:09 -0700

    add home header component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import HomeHeader from './components/HomeHeader'
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <HomeHeader />
+    </>
   )
 }
