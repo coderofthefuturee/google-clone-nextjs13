8807f1fee263749e8a0c7ca096368706d2317839
Prince <princeking950@gmail.com>
2023-09-13 18:07:58 -0700

    create search page for web and image

--- src/app/components/HomeSearch.jsx
+++ src/app/components/HomeSearch.jsx
@@ -12,22 +12,27 @@ export default function HomeSearch() {
     function handleSubmit(e) {
         e.preventDefault();
         if(!input.trim()) return;
-        // router.push(`/search/web?searchTerm=${input}`);
-        router.push(`https://www.google.com/search?q=${input}`);
+        //Clone Google search
+        router.push(`/search/web?searchTerm=${input}`);
+        //Google Search
+        // router.push(`https://www.google.com/search?q=${input}`);
     }
     async function randomSearch() {
         setRandomSearchLoading(true);
         const response = await fetch("https://random-word-api.herokuapp.com/word")
             .then((res) => res.json())
             .then((data) => data[0]);
-            // router.push(`/search/web?searchTerm=${response}`);
-        router.push(`https://www.google.com/search?q=${response}`);
+        if (!response) return;
+        //Clone Google search
+        router.push(`/search/web?searchTerm=${response}`);
+        //Google Search
+        // router.push(`https://www.google.com/search?q=${response}`);
         setRandomSearchLoading(false);
     }
     return (
-    <>
+        <>
         <form onSubmit={handleSubmit}
-        className='flex w-full m5-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl'>
+            className='flex w-full m5-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl'>
             < AiOutlineSearch className='text-xl text-gray-500 mr-3'/>
             <input 
                 type="text" 
@@ -35,6 +40,7 @@ export default function HomeSearch() {
                 onChange={(e) => setInput(e.target.value)}/>
             <BsFillMicFill />
         </form>
+
         <div className='flex space-x-4'>
             <button onClick={handleSubmit}
             className="flex rounded-full p-2 flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8 border border-white hover:bg-blue-600 transition duration-200">Google Search</button>
